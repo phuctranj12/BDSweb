ee3b1fc25ea49b1c056ebbec05eebf25f0158c16
phuctranj12 <tranphuc120203@gmail.com>
2024-11-09 21:49:45 +0700

    LAST FIX

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -17,7 +17,7 @@ function Footer({ }) {
             <div className="footer-row2">
                 <div className="footer-tieude">Công ty</div>
                 <div className="footer-noidung">
-                    <a href="">Về chúng tôi</a><br />
+                    <a href='https://www.facebook.com/batdongsantruongphatquangngai/' target="_blank">Về chúng tôi</a><br />
                     <a href="/Policy">Chính sách bảo mật</a><br />
                     <a href="/Policy">Chính sách đặt cọc</a><br />
                     <a href="/Policy">Chính sách hoàn trả</a><br />
@@ -28,12 +28,14 @@ function Footer({ }) {
             <div className="footer-row3">
                 <div className="footer-tieude">HOTLINE</div>
                 <div className="lienhe1">
-                    1900 9999
+                    0965561991
                 </div>
                 <div className="icon-contain">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
-                        <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
-                    </svg>
+                    <a href='https://www.facebook.com/batdongsantruongphatquangngai/' target="_blank">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
+                        </svg>
+                    </a>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tiktok" viewBox="0 0 16 16">
                         <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z" />
                     </svg>
